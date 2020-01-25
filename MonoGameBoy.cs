997ae5b2b482c403909dbc6b7abce623d2a7c4d5
taylus <brandon@legacy.codes>
2020-01-25 12:23:17 -0600

    Update to load and display new pixel data from gbdotnet

--- MonoGameBoy.cs
+++ MonoGameBoy.cs
@@ -77,7 +77,7 @@ namespace MonoGameBoy
             screen = new GameBoyScreen(GraphicsDevice, width: 128, height: 192);
             SetWindowSize(screen.Width * 4, screen.Height * 4);
 
-            string inputFile = Path.Combine("input", "tetris_tileset_pixels.bin");
+            string inputFile = Path.Combine("input", "pokemon_reds_room_tileset.bin");
             screen.PutPixelsFromFile(palette, inputFile);
 
             Window.Title = $"MonoGameBoy - Tiles [{inputFile}]";
@@ -88,7 +88,7 @@ namespace MonoGameBoy
             screen = new GameBoyScreen(GraphicsDevice, width: 256, height: 256);
             SetWindowSize(screen.Width * 2, screen.Height * 2);
 
-            string inputFile = Path.Combine("input", "tetris_bgmap_pixels.bin");
+            string inputFile = Path.Combine("input", "pokemon_reds_room_bgmap.bin");
             screen.PutPixelsFromFile(palette, inputFile);
 
             Window.Title = $"MonoGameBoy - Background Map [{inputFile}]";
@@ -99,7 +99,7 @@ namespace MonoGameBoy
             screen = new GameBoyScreen(GraphicsDevice, width: 160, height: 144);
             SetWindowSize(512, 512);
 
-            string inputFile = Path.Combine("input", "tetris_sprite_pixels.bin");
+            string inputFile = Path.Combine("input", "pokemon_reds_room_sprites.bin");
             screen.PutPixelsFromFile(palette, inputFile);
 
             Window.Title = $"MonoGameBoy - Sprite Layer [{inputFile}]";
@@ -113,8 +113,8 @@ namespace MonoGameBoy
         protected override void Draw(GameTime gameTime)
         {
             spriteBatch.Begin(samplerState: SamplerState.PointClamp);
-            var sourceRect = Window.Title.Contains("Background Map") ? new Rectangle(0, 0, 160, 144) : (Rectangle?)null;    //HACK: implement scroll registers later
-            screen.Draw(spriteBatch, GraphicsDevice.Viewport.Bounds, sourceRect);
+            //var sourceRect = Window.Title.Contains("Background Map") ? new Rectangle(0, 0, 160, 144) : (Rectangle?)null;    //HACK: implement scroll registers later
+            screen.Draw(spriteBatch, GraphicsDevice.Viewport.Bounds);
             spriteBatch.End();
 
             base.Draw(gameTime);
